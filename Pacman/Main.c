470aa75f762299469cc87c7c8f26b2ea3a6c155a
Markus Reynolds <markusreynolds1989@gmail.com>
2020-01-11 19:37:11 -0600

    Removed Pellets, added Ghost ai.

--- Pacman/Main.c
+++ Pacman/Main.c
@@ -6,31 +6,38 @@
 typedef struct Player {
 	Vector2 position;
 	Vector2 speed;
+	int size;
 } Player;
 
 typedef struct Pellet {
 	Vector2 position;
+	int size;
 } Pellet;
 
 typedef struct Ghost {
 	Vector2 position;
 	Vector2 speed;
+	int size;
 	Color color;
 } Ghost;
+
+enum playerSpeed {
+	up = -2,
+	down = 2,
+	right = 2,
+	left = -2,
+};
+
 
 void UpdateGame();
 void DrawGame();
 void InitGame();
 void UpdateDrawFrame();
-
 int32_t gameOver = 1;
 int32_t score = 0;
 static Player player = { 0 };
 static Ghost pinky = { 0 };
-//Array of structs
-Pellet pellets[25] = { 0 };
 uint8_t* scoreBuffer;
-
 int32_t main() {
 	InitWindow(600, 800, "Pacman");
 	SetTargetFPS(60);
@@ -43,25 +50,27 @@ int32_t main() {
 }
 
 void InitGame() {
+	/* Player */
 	player.position = (Vector2){ 100,100 };
 	player.speed = (Vector2){ 0,0 };
-	for (int32_t i = 5; i < 25; i++)
-	{
-		pellets[i].position = (Vector2){ 20, i * 30 };
-	}
+	player.size = 10;
+	/* Generate pellets/Power pellet */
+	/* Ghosts */
 	pinky.color = PINK;
 	pinky.speed = (Vector2){ 0,0 };
+	pinky.size = 10;
 	pinky.position = (Vector2){ 300,400 };
 }
 
 /* Main Event Handler */
 void UpdateGame() {
+	enum speed speed;
 	if (gameOver > 0) {
-		/* Init Speed */
 		player.position.y += player.speed.y;
 		player.position.x += player.speed.x;
 		pinky.position.y += pinky.speed.y;
 		pinky.position.x += pinky.speed.x;
+
 		/* Event Handler */
 		if (IsKeyDown(KEY_DOWN)) {
 			if (player.speed.y != 2) player.speed.y += 2;
@@ -80,39 +89,65 @@ void UpdateGame() {
 			player.speed.y = 0;
 		}
 		/* Player Move Logic */
+		/* Holes */
+		/* Top Left: X 3 Y 46
+		Top Right : X 585 Y 49
+		Bottom Left : X 2 Y : 779
+		Bottom Right : X 590 Y 775 */
+
 		if (player.position.y >= 782) player.position.y = 781;
 		if (player.position.x >= 582) player.position.x = 581;
 		if (player.position.y <= 50) player.position.y = 51;
 		if (player.position.x <= 12) player.position.x = 13;
-
+		
 		/* Ghost Move Logic */
-		if (pinky.position.y >= 800) pinky.position.y = 0;
-		if (pinky.position.x >= 600) pinky.position.x = 0;
-		if (pinky.position.y <= -1) pinky.position.y = 800;
-		if (pinky.position.x <= -1) pinky.position.x = 600;
+		if (pinky.position.y >= 782) pinky.position.y = 781;
+		if (pinky.position.x >= 582) pinky.position.x = 581;
+		if (pinky.position.y <= 50) pinky.position.y = 51;
+		if (pinky.position.x <= 12) pinky.position.x = 13;
 
-		/* Pellet Logic */
+		/* Pellet Logic 
 		for (int32_t i = 0; i < 25; i++)
 		{
-			if ((player.position.x < (pellets[i].position.x + 5)
-				&& (player.position.x + 10) > pellets[i].position.x
-				&& player.position.y < (pellets[i].position.y + 5)
-				&& player.position.y + 10>(pellets[i].position.y)))
+			if ((player.position.x < (pellets[i].position.x + pellets[i].size)
+				&& (player.position.x + player.size) > pellets[i].position.x
+				&& player.position.y < (pellets[i].position.y + pellets[i].size)
+				&& player.position.y + player.size >(pellets[i].position.y)))
 			{
 				pellets[i].position = (Vector2){ -5,-5 };
 				score++;
 			}
-		}
-		/* Collison with pinky the ghost */
-		if ((player.position.x < (pinky.position.x + 10)
-			&& (player.position.x + 10) > pinky.position.x
-			&& player.position.y < (pinky.position.y + 10)
-			&& player.position.y + 10>(pinky.position.y))) gameOver = 0;
+		} */
 
-		/* Pinky Follow :ogic? */
+		/* Collison with pinky the ghost */
+		if ((player.position.x < (pinky.position.x + player.size)
+			&& (player.position.x + player.size) > pinky.position.x
+			&& player.position.y < (pinky.position.y + pinky.size)
+			&& player.position.y + player.size >(pinky.position.y))) gameOver = 0;
+
+		/* Pinky Follow Logic? */
+		if (pinky.position.x >= player.position.x
+			&& pinky.position.y >= player.position.y) {
+			pinky.speed.x = -1.5;
+			pinky.speed.y = -1.5;
+		}
+		if (pinky.position.x <= player.position.x
+			&& pinky.position.y <= player.position.y) {
+			pinky.speed.x = 1.5;
+			pinky.speed.y = 1.5;
+		}
+		if (pinky.position.x <= player.position.x
+			&& pinky.position.y >= player.position.y) {
+			pinky.speed.x = 1.5;
+			pinky.speed.y = -1.5;
+		}
+		if (pinky.position.x >= player.position.x
+			&& pinky.position.y <= player.position.y) {
+			pinky.speed.x = -1.5;
+			pinky.speed.y = 1.5;
+		}
 	}
 }
-
 void DrawGame() {
 	/* Drawing */
 	BeginDrawing();
@@ -129,18 +164,19 @@ void DrawGame() {
 	/*End*/
 
 	/*Ghost Box*/
+
+
 	/*End*/
 	scoreBuffer = malloc(32);
+
+	//Init
 	if (gameOver > 0) {
-		DrawCircleV(player.position, 10, YELLOW);
-		for (int32_t i = 0; i < 25; i++)
-		{
-			DrawCircleV(pellets[i].position, 4, WHITE);
-		}
+		DrawCircleV(player.position, player.size, YELLOW);
 		sprintf_s(scoreBuffer, 32, "Score:%d", score);
 		DrawText(scoreBuffer, 480, 10, 20, WHITE);
-		DrawCircleV(pinky.position, 10, pinky.color);
+		DrawCircleV(pinky.position, pinky.size, pinky.color);
 	}
+
 	if (gameOver == 0) {
 		DrawText("Game Over", 250, 400, 20, WHITE);
 	}
