1f03349e6af329d73b2c195a17f1dfc3ae7e5105
Markus Reynolds <markus.reynolds1989@gmail.com>
2020-01-13 16:08:49 -0600

    Walls, Collision, and more

--- Pacman/Main.c
+++ Pacman/Main.c
@@ -3,24 +3,27 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+
 typedef struct Player {
 	Vector2 position;
 	Vector2 speed;
 	int size;
 } Player;
-
 typedef struct Pellet {
 	Vector2 position;
 	int size;
 } Pellet;
-
+typedef struct Wall {
+	Vector2 startPos;
+	Vector2 endPos;
+	float width;
+}Wall;
 typedef struct Ghost {
 	Vector2 position;
 	Vector2 speed;
 	int size;
 	Color color;
 } Ghost;
-
 enum playerSpeed {
 	up = -2,
 	down = 2,
@@ -28,16 +31,24 @@ enum playerSpeed {
 	left = -2,
 };
 
-
 void UpdateGame();
 void DrawGame();
 void InitGame();
 void UpdateDrawFrame();
+void EventHandler();
+void CheckPlayerWall();
 int32_t gameOver = 1;
 int32_t score = 0;
 static Player player = { 0 };
 static Ghost pinky = { 0 };
+const int wallBuffer = 2;
+const int lineBuffer = 3;
+static Wall topWall = { 44 , 64, 554, 64, 12 };
+static Wall leftWall = { 4, 58, 4, 800,  12 };
+static Wall rightWall = { 594, 58, 594, 800,  12 };
+static Wall bottomWall = { 44 , 794, 554, 794, 12 };
 uint8_t* scoreBuffer;
+
 int32_t main() {
 	InitWindow(600, 800, "Pacman");
 	SetTargetFPS(60);
@@ -70,101 +81,28 @@ void UpdateGame() {
 		player.position.x += player.speed.x;
 		pinky.position.y += pinky.speed.y;
 		pinky.position.x += pinky.speed.x;
-
-		/* Event Handler */
-		if (IsKeyDown(KEY_DOWN)) {
-			if (player.speed.y != 2) player.speed.y += 2;
-			player.speed.x = 0;
-		}
-		if (IsKeyDown(KEY_UP)) {
-			if (player.speed.y != -2) player.speed.y -= 2;
-			player.speed.x = 0;
-		}
-		if (IsKeyDown(KEY_LEFT)) {
-			if (player.speed.x != -2) player.speed.x -= 2;
-			player.speed.y = 0;
-		}
-		if (IsKeyDown(KEY_RIGHT)) {
-			if (player.speed.x != 2) player.speed.x += 2;
-			player.speed.y = 0;
-		}
-		/* Player Move Logic */
-		/* Holes */
-		/* Top Left: X 3 Y 46
-		Top Right : X 585 Y 49
-		Bottom Left : X 2 Y : 779
-		Bottom Right : X 590 Y 775 */
-
-		if (player.position.y >= 782) player.position.y = 781;
-		if (player.position.x >= 582) player.position.x = 581;
-		if (player.position.y <= 50) player.position.y = 51;
-		if (player.position.x <= 12) player.position.x = 13;
-		
-		/* Ghost Move Logic */
-		if (pinky.position.y >= 782) pinky.position.y = 781;
-		if (pinky.position.x >= 582) pinky.position.x = 581;
-		if (pinky.position.y <= 50) pinky.position.y = 51;
-		if (pinky.position.x <= 12) pinky.position.x = 13;
-
-		/* Pellet Logic 
-		for (int32_t i = 0; i < 25; i++)
-		{
-			if ((player.position.x < (pellets[i].position.x + pellets[i].size)
-				&& (player.position.x + player.size) > pellets[i].position.x
-				&& player.position.y < (pellets[i].position.y + pellets[i].size)
-				&& player.position.y + player.size >(pellets[i].position.y)))
-			{
-				pellets[i].position = (Vector2){ -5,-5 };
-				score++;
-			}
-		} */
-
-		/* Collison with pinky the ghost */
-		if ((player.position.x < (pinky.position.x + player.size)
-			&& (player.position.x + player.size) > pinky.position.x
-			&& player.position.y < (pinky.position.y + pinky.size)
-			&& player.position.y + player.size >(pinky.position.y))) gameOver = 0;
-
-		/* Pinky Follow Logic? */
-		if (pinky.position.x >= player.position.x
-			&& pinky.position.y >= player.position.y) {
-			pinky.speed.x = -1.5;
-			pinky.speed.y = -1.5;
-		}
-		if (pinky.position.x <= player.position.x
-			&& pinky.position.y <= player.position.y) {
-			pinky.speed.x = 1.5;
-			pinky.speed.y = 1.5;
-		}
-		if (pinky.position.x <= player.position.x
-			&& pinky.position.y >= player.position.y) {
-			pinky.speed.x = 1.5;
-			pinky.speed.y = -1.5;
-		}
-		if (pinky.position.x >= player.position.x
-			&& pinky.position.y <= player.position.y) {
-			pinky.speed.x = -1.5;
-			pinky.speed.y = 1.5;
-		}
+		EventHandler();
 	}
 }
+
 void DrawGame() {
 	/* Drawing */
 	BeginDrawing();
 	ClearBackground(BLACK);
+
 	/*Walls */
-	/*Left Wall*/
-	DrawRectangle(0, 64, 8, 700, PURPLE);
 	/*Top Wall*/
-	DrawRectangle(0, 34, 600, 8, PURPLE);
+	DrawLineEx(topWall.startPos, topWall.endPos, topWall.width, PURPLE);
+	/*Left Wall*/
+	DrawLineEx(leftWall.startPos, leftWall.endPos, leftWall.width, PURPLE);
 	/*Bottom Wall*/
-	DrawRectangle(0, 792, 600, 8, PURPLE);
+	DrawLineEx(bottomWall.startPos, bottomWall.endPos, bottomWall.width, PURPLE);
 	/*Right Wall*/
-	DrawRectangle(592, 64, 8, 700, PURPLE);
+	DrawLineEx(rightWall.startPos, rightWall.endPos, rightWall.width, PURPLE);
 	/*End*/
 
 	/*Ghost Box*/
-
+	DrawRectangleLines(250, 350, 100, 100, PURPLE);
 
 	/*End*/
 	scoreBuffer = malloc(32);
@@ -187,4 +125,90 @@ void DrawGame() {
 void UpdateDrawFrame() {
 	UpdateGame();
 	DrawGame();
+}
+
+void CheckPlayerWall() {
+	/* Player Wall Collision Logic */
+	if (player.position.x <= leftWall.startPos.x + leftWall.width + wallBuffer) {
+		player.position.x = leftWall.width + leftWall.startPos.x + lineBuffer;
+	}
+	if (player.position.x >= rightWall.startPos.x - rightWall.width - wallBuffer) {
+		player.position.x = rightWall.startPos.x - rightWall.width - lineBuffer;
+	}
+	if (player.position.y <= topWall.startPos.y + topWall.width + wallBuffer) {
+		player.position.y = topWall.startPos.y + topWall.width + lineBuffer;
+	}
+	if (player.position.y >= bottomWall.startPos.y - bottomWall.width - wallBuffer) {
+		player.position.y = bottomWall.startPos.y - bottomWall.width - lineBuffer;
+	}
+}
+
+void EventHandler() {
+	/* Event Handler */
+	if (IsKeyDown(KEY_DOWN)) {
+		if (player.speed.y != 2) player.speed.y += 2;
+		player.speed.x = 0;
+	}
+	if (IsKeyDown(KEY_UP)) {
+		if (player.speed.y != -2) player.speed.y -= 2;
+		player.speed.x = 0;
+	}
+	if (IsKeyDown(KEY_LEFT)) {
+		if (player.speed.x != -2) player.speed.x -= 2;
+		player.speed.y = 0;
+	}
+	if (IsKeyDown(KEY_RIGHT)) {
+		if (player.speed.x != 2) player.speed.x += 2;
+		player.speed.y = 0;
+	}
+
+	CheckPlayerWall();
+
+
+	/* Ghost Move Logic */
+	if (pinky.position.y >= 782) pinky.position.y = 781;
+	if (pinky.position.x >= 582) pinky.position.x = 581;
+	if (pinky.position.y <= 50) pinky.position.y = 51;
+	if (pinky.position.x <= 12) pinky.position.x = 13;
+
+	/* Pellet Logic
+	for (int32_t i = 0; i < 25; i++)
+	{
+		if ((player.position.x < (pellets[i].position.x + pellets[i].size)
+			&& (player.position.x + player.size) > pellets[i].position.x
+			&& player.position.y < (pellets[i].position.y + pellets[i].size)
+			&& player.position.y + player.size >(pellets[i].position.y)))
+		{
+			pellets[i].position = (Vector2){ -5,-5 };
+			score++;
+		}
+	} */
+
+	/* Collison with pinky the ghost */
+	if ((player.position.x < (pinky.position.x + player.size)
+		&& (player.position.x + player.size) > pinky.position.x
+		&& player.position.y < (pinky.position.y + pinky.size)
+		&& player.position.y + player.size >(pinky.position.y))) gameOver = 0;
+
+	/* Pinky Follow Logic?
+	if (pinky.position.x >= player.position.x
+		&& pinky.position.y >= player.position.y) {
+		pinky.speed.x = -1.5;
+		pinky.speed.y = -1.5;
+	}
+	if (pinky.position.x <= player.position.x
+		&& pinky.position.y <= player.position.y) {
+		pinky.speed.x = 1.5;
+		pinky.speed.y = 1.5;
+	}
+	if (pinky.position.x <= player.position.x
+		&& pinky.position.y >= player.position.y) {
+		pinky.speed.x = 1.5;
+		pinky.speed.y = -1.5;
+	}
+	if (pinky.position.x >= player.position.x
+		&& pinky.position.y <= player.position.y) {
+		pinky.speed.x = -1.5;
+		pinky.speed.y = 1.5;
+	} */
 }
